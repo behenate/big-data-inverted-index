af7613ac912fc0adde7796b5804ec75eb5f251e9
Jakub-44-Kr <jakub.44.kr@gmail.com>
2024-10-17 22:19:43 +0200

    Benchmarks :D

---
+++ test/test_benchmark.py
@@ -0,0 +1,19 @@
+import pytest
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../indexer')))
+
+from indexer import run_indexer  # Upewnij się, że importujesz poprawnie
+
+def test_indexing_time(benchmark):
+    # Funkcja uruchamiająca proces indeksowania
+    def run_index():
+        run_indexer()  # Uruchamiamy funkcję, która indeksuje książki
+
+    # Mierzymy czas wykonania funkcji run_index
+    benchmark(run_index)  # Używamy benchmark bez przypisywania wyniku
+
+
+
