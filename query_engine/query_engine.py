3e6a974a63a21302fb7f65052772b10dc8733021
Olga Kalisiak <01178853@pw.edu.pl>
2024-10-17 17:45:28 +0100

    update query_engine

--- query_engine/query_engine.py
+++ query_engine/query_engine.py
@@ -9,27 +9,36 @@ def search_docs_from_db(keyword, db_path='../databases/inverted_index.db'):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    cursor.execute("SELECT doc_ids FROM inverted_index WHERE term = ?", (keyword,))
-    result = cursor.fetchone()
+    query = """
+    SELECT w.word, wo.book_id, wo.frequency, p.position 
+    FROM Words w
+    JOIN WordOccurrences wo ON w.id = wo.word_id
+    JOIN Positions p ON wo.id = p.occurrence_id
+    WHERE w.word = ?
+    """
+    cursor.execute(query, (keyword,))
+    result = cursor.fetchall()
 
     conn.close()
 
     if result:
-        doc_ids = list(map(int, result[0].split(',')))
-        return doc_ids
+        books_data = {}
+        for word, book_id, frequency, position in result:
+            if book_id not in books_data:
+                books_data[book_id] = {'frequency': frequency, 'positions': []}
+            books_data[book_id]['positions'].append(position)
+        return books_data
     else:
         return None
 
 
-def fetch_books_details(doc_ids, db_path='../databases/books.db'):
-
-    # Connection to database
+def fetch_books_details(book_ids, db_path='../databases/books.db'):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    placeholders = ', '.join('?' for _ in doc_ids)
+    placeholders = ', '.join('?' for _ in book_ids)
     query = f"SELECT id, author, editor, release, language FROM books WHERE id IN ({placeholders})"
-    cursor.execute(query, doc_ids)
+    cursor.execute(query, book_ids)
 
     books = cursor.fetchall()
 
@@ -51,18 +60,23 @@ def fetch_books_details(doc_ids, db_path='../databases/books.db'):
 
 word_to_search = input("Keyword to search: ")
 
-doc_ids = search_docs_from_db(word_to_search)
+books_data = search_docs_from_db(word_to_search)
 
-if doc_ids:
-    print(f"IDs found for word '{word_to_search}': {doc_ids}")
+if books_data:
+    book_ids = list(books_data.keys())
+    print(f"IDs found for word '{word_to_search}': {book_ids}")
 
-    books_details = fetch_books_details(doc_ids)
+    books_details = fetch_books_details(book_ids)
 
     if books_details:
         print("Books details:")
         for book in books_details:
-            print(f"ID: {book['id']}, Author: {book['author']}, Edithor: {book['editor']}, "
-                  f"Release date: {book['release']}, Language: {book['language']}")
+            book_id = book['id']
+            frequency = books_data[book_id]['frequency']
+            positions = books_data[book_id]['positions']
+            print(f"ID: {book['id']}, Author: {book['author']}, Editor: {book['editor']}, "
+                  f"Release date: {book['release']}, Language: {book['language']}, "
+                  f"Frequency: {frequency}, Positions: {positions}")
     else:
         print("No books for found IDs.")
 else:
